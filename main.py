8a0fdf9476731e59d4b534a3412dd97895fc7612
tommaso <tommaso.dognini@gmail.com>
2022-01-28 16:35:07 +0100

    aggiunto is_lint

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from picamera import PiCamera
 import cv2
 import numpy as np
 from fastiecm import fastiecm
+from orbit import ISS
+from skyfield.api import load
+
 
 # function to increase the contrast
 def contrast(img):
@@ -29,7 +32,16 @@ def calc_ndvi (img):
     ndvi = (b.astype(float) - r) / bottom
     return ndvi
 
-#base_folder = Path(__file__).parent.resolve()
+
+
+ephemeris = load('de421.bsp')
+timescale = load.timescale()
+
+while True:
+    t = timescale.now()
+    if ISS.at(t).is_sunlit(ephemeris):
+        #there is light: we run our experiment
+        #base_folder = Path(__file__).parent.resolve()
 
 camera = PiCamera()
 camera.resolution = (1296,972) #qui dobbiamo scegliere la risoluzione delle immagini che vogliamo 
@@ -47,6 +59,15 @@ for i in range(1):   #all'interno dell' range dobbiamo scegliere quante foto far
     color_mapped_prep = ndvi_contrasted.astype(np.uint8)
     color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
     cv2.imwrite("img/imageNdvi%s.jpg" % i, color_mapped_image)
+
+
+
+    else:
+        #is dark
+
+
+
+
 
 
 
