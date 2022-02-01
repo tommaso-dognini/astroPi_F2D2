39a363cb48930e69dd62bff4361b43e811508aba
Tommaso Dognini <tommaso.dognini@gmail.com>
2022-02-01 09:52:02 +0100

    cambiato nuemro di foto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#code that makes the photos
+# code that makes the photos
 from time import sleep
 from picamera import PiCamera
 # from pathlib import Path
@@ -21,17 +21,18 @@ def contrast(img):
     return out
 
 # function to culculate the ndvi index
-def calc_ndvi (img):
-    #create the variabels for the 3 colors channels: red. green and blue
+
+
+def calc_ndvi(img):
+    # create the variabels for the 3 colors channels: red. green and blue
     b, g, r = cv2.split(img)
-    # create bottom as the sum of the red and blue channel 
+    # create bottom as the sum of the red and blue channel
     bottom = (r.astype(float) + b.astype(float))
-    #initialize bottom
-    bottom[bottom==0] = 0.01
+    # initialize bottom
+    bottom[bottom == 0] = 0.01
     # calculate the ndvi index
     ndvi = (b.astype(float) - r) / bottom
     return ndvi
-
 
 
 ephemeris = load('de421.bsp')
@@ -40,16 +41,17 @@ timescale = load.timescale()
 while True:
     t = timescale.now()
     if ISS.at(t).is_sunlit(ephemeris):
-        #there is light: we run our experiment
+        # there is light: we run our experiment
         #base_folder = Path(__file__).parent.resolve()
 
         camera = PiCamera()
-        camera.resolution = (1296,972) #qui dobbiamo scegliere la risoluzione delle immagini che vogliamo 
+        # qui dobbiamo scegliere la risoluzione delle immagini che vogliamo
+        camera.resolution = (1296, 972)
         camera.start_preview()
-        for i in range(1):   #all'interno dell' range dobbiamo scegliere quante foto fare scattare al programma durante le tre ore
+        for i in range(10):  # all'interno dell' range dobbiamo scegliere quante foto fare scattare al programma durante le tre ore
             # Camera warm-up time
             sleep(2)
-            camera.capture("img/image%s.jpg" % i) 
+            camera.capture("img/image%s.jpg" % i)
             # load the original img
             original = cv2.imread("imgage%s.jpg" % i)
             contrasted = contrast(original)
@@ -59,16 +61,6 @@ while True:
             color_mapped_prep = ndvi_contrasted.astype(np.uint8)
             color_mapped_image = cv2.applyColorMap(color_mapped_prep, fastiecm)
             cv2.imwrite("img/imageNdvi%s.jpg" % i, color_mapped_image)
-            
+
     else:
-        #is dark
-
-
-
-
-
-
-
-
-
-
+        # is dark
